f4c79778a9f90fd49b55b290e2a48f4742092d9f
Ana Paula Vale <ana.vale@talkdesk.com>
2021-08-22 06:56:20 +0100

    Change index message

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-require('dotenv').config({ silent: process.env.NODE_ENV === 'production' })
-
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
